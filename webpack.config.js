c5fba188a112bfa41b9b52144fc1e6ce722d8eac
Edmo Lima <devedmo@gmail.com>
2016-05-08 21:57:09 -0300

    Add badge bootstrap component.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "lib"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./index.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/dist/",
+    filename: "badge.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
